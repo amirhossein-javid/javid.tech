c44c8d7c7ebe1e4e3ec46b68b86e91dcbedf356a
Amirhosein Javid <amirhosein.javidd@gmail.com>
2025-09-26 19:45:03 +0330

    feat: add ContactMessageForm

---
+++ pages/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+
+from .models import ContactMessage
+
+
+class ContactMessageForm(forms.ModelForm):
+    class Meta:
+        model = ContactMessage
+        fields = ['name', 'email', 'subject', 'message']
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+            'subject': forms.TextInput(attrs={'class': 'form-control'}),
+            'message': forms.Textarea(attrs={'class': 'form-control', 'rows': 5}),
+        }
+        
