5c88f21285846cb3232d02c2975ebbca799e2095
Amirhosein Javid <amirhosein.javidd@gmail.com>
2025-09-26 19:40:28 +0330

    feat: add ServicesWebDevelopmentView

--- pages/views.py
+++ pages/views.py
@@ -3,4 +3,7 @@ from django.views.generic import TemplateView
 
 class HomeView(TemplateView):
     template_name = "pages/home.html"
-    
+
+
+class ServicesWebDevelopmentView(TemplateView):
+    template_name = "pages/services_web_development.html"
