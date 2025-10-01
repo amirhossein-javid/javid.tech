8b602793deffffa2f30398e009c7b6e2978d3888
Amirhosein Javid <amirhosein.javidd@gmail.com>
2025-10-01 07:03:09 +0330

    chore: configure PostgreSQL database

--- config/settings.py
+++ config/settings.py
@@ -80,7 +80,14 @@ WSGI_APPLICATION = "config.wsgi.application"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    "default": env.db(default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}")
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": env("DB_NAME"),
+        "USER": env("DB_USER"),
+        "PASSWORD": env("DB_PASSWORD"),
+        "HOST": env("DB_HOST", default="localhost"),
+        "PORT": env("DB_PORT", default="5432"),
+    }
 }
 
 
